1c978e87b99d45a99811e532ba0f91bdfc275809
Edikan A <73974392+SoberandVigilant@users.noreply.github.com>
2022-03-25 12:22:39 -0400

    event listener

--- script/script.js
+++ script/script.js
@@ -9,3 +9,26 @@ window.onscroll = function () {
         header.classList.remove('navbarDark');
     }
 }
+
+const form = document.querySelector('.content_form');
+
+function sendMsg(e) {
+    e.preventDefault();
+ 
+        const name = document.querySelector('.name'),
+              email = document.querySelector('.email'),
+               msg = document.querySelector('.msg');
+
+    Email.send({
+        SecureToken: "9ECA920BBFCA28F117DF60B7DDC1FA696479",
+        To: 'edikaneakpan1996@gmail.com',
+        From: email.value,
+        Subject: "Contact form",
+        Body: msg.value
+    }).then(
+        message => alert(message)
+    );
+}
+
+
+form.addEventListener('submit', sendMsg);
